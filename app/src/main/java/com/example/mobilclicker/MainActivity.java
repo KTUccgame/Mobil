0cf51b2eaa8d40e0ddb3bc073ad9a2a7d8d113e2
B-Rakevicius <115837262+B-Rakevicius@users.noreply.github.com>
2025-03-17 12:19:41 +0200

    button+points
    Added a circle to the first activity, on click updates textbox with the number of score/clicks done this session.

--- app/src/main/java/com/example/mobilclicker/MainActivity.java
+++ app/src/main/java/com/example/mobilclicker/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.mobilclicker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button _button;
+    TextView _textview;
+    int score = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
+        });
+        _button = findViewById((R.id.button));
+        _textview = findViewById(R.id.textview);
+        _button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                _textview.setText("" + score++); // (""+score) works, (score) doesn't
+                Log.i("i","onclick " + score); // onClick
+                // call update points here
+            }
         });
     }
 }
